be5b6998280f690eeeae244e730d754456799d9f
sammy wlodawski <samwlodawski@gmail.com>
2024-08-01 16:24:58 -0400

    Adding inquirer questions and fswrite logic to index.js file.

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 fs = require('fs');
 inquirer = require('inquirer');
 
+const { default: inquirer } = require('inquirer');
 const {Circle, Square, Triangle} = require('./lib/shapes');
 
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter a maximum of 3 characters',
+        validate: (text) => {
+            if(text.length <= 3) {
+                return(true) 
+            } else {return('Enter up to 3 letters')}
+        }
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter a maximum of 3 characters'},
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter a maximum of 3 characters'}
+];
 
+inquirer.prompt(questions).then((answers) => {
+    
+})
