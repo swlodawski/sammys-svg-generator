71ef95137a144f8b036d2d2dacb2474344711f69
sammy wlodawski <samwlodawski@gmail.com>
2024-08-02 13:09:39 -0400

    Just tabbing code.

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const {Circle, Square, Triangle} = require('./lib/shapes');
+    const fs = require('fs');
+    const inquirer = require('inquirer');
+    const {Circle, Square, Triangle} = require('./lib/shapes');
 
-const questions = [
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Enter a maximum of 3 characters',
-        validate: (text) => {
-            if(text.length <= 3) {
+    const questions = [
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter a maximum of 3 characters',
+            validate: (text) => {
+                if(text.length <= 3) {
                 return(true) 
-            } else {return('Enter up to 3 letters')}
+                } else {return('Enter up to 3 letters')}
+            }
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Enter a maximum of 3 charactersPick a shape',
+            choices: [Circle, Square, Triangle
+            ]},
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter a shape color'}
+    ];
+
+    inquirer.prompt(questions).then((answers) => {
+        let shape;
+
+        if(answers.shape === 'circle') {
+            shape = new Circle()
         }
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Enter a maximum of 3 charactersPick a shape',
-        choices: [Circle, Square, Triangle
-        ]},
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Enter a shape color'}
-];
-
-inquirer.prompt(questions).then((answers) => {
-    let shape;
-
-    if(answers.shape === 'circle') {
-        shape = new Circle()
-    }
     
-    if(answers.shape === 'square') {
-        shape = new Square()
-    }
+        if(answers.shape === 'square') {
+            shape = new Square()
+        }
     
-    if(answers.shape === 'triangle') {
-        shape = new Triangle()
-    }
-    shape.setColor(answers.shapeColor)
-})
+        if(answers.shape === 'triangle') {
+            shape = new Triangle()
+        }
+        shape.setColor(answers.shapeColor)
+    })
