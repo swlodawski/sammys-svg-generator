2a685e2da58027057ed6265f026421096009888b
sammy wlodawski <samwlodawski@gmail.com>
2024-08-02 15:06:07 -0400

    Added various changes to packages. shapes.js and index.js logic.

--- index.js
+++ index.js
@@ -10,41 +10,49 @@
             validate: (text) => {
                 if(text.length <= 3) {
                 return(true) 
-                } else {return('Enter up to 3 letters')}
+                } else {return'Enter up to 3 letters'}
             }
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter a text color'
         },
         {
             type: 'list',
             name: 'shape',
-            message: 'Enter a maximum of 3 charactersPick a shape',
-            choices: [Circle, Square, Triangle
-            ]},
+            message: 'Select a shape',
+            choices: ['circle', 'square', 'triangle'],
+        },
         {
             type: 'input',
             name: 'shapeColor',
-            message: 'Enter a shape color'}
+            message: 'Enter a shape color'},
     ];
 
-    inquirer.prompt(questions).then((answers) => {
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
         let shape;
 
         if(answers.shape === 'circle') {
-            shape = new Circle()
+            shape = new Circle();
         }
     
         if(answers.shape === 'square') {
-            shape = new Square()
+            shape = new Square();
         }
     
         if(answers.shape === 'triangle') {
-            shape = new Triangle()
+            shape = new Triangle();
         }
-        shape.setColor(answers.shapeColor)
-    })
+        shape.setColor(answers.shapeColor);
 
-    const svgFile = `<svg-version="1.1" height="200" width="300" xmlns="http://www.w3.org/2000/svg"> ${shape.render()} <text x="150" y="125" fonst-size="60" text-anchor="middle" fill=${answers.textColor}">${answers.text}</text></svg>`
+    const svgFile = `<svg-version="1.1" height="200" width="300" xmlns="http://www.w3.org/2000/svg"> ${shape.render()} <text x="150" y="125" fonst-size="60" text-anchor="middle" fill=${answers.textColor}">${answers.text}</text></svg>`;
 
-    fs.writeFile('SVG-Generator.svg', svgFile), (err) => {
+    fs.writeFile('SVG-Generator.svg', svgFile, (err) => {
         if(err) throw err;
-        console.log('File created successfully.')
-    };
+        console.log('File created successfully.');
+
+    });
+});
