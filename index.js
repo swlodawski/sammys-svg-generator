0d2d404330b5dcc0df20b42d528806f6d55c2e28
sammy wlodawski <samwlodawski@gmail.com>
2024-08-07 16:02:06 -0400

    Testing url to generate svg file.

--- index.js
+++ index.js
@@ -48,7 +48,11 @@
         }
         shape.setColor(answers.shapeColor);
 
-    const svgFile = `<svg-version="1.1" height="200" width="300" xmlns="http://www.w3.org/2000/svg"> ${shape.render()} <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text></svg>`;
+    // const svgFile = `<svg-version="1.1" height="200" width="300" xmlns="http://www.w3.org/2000/svg"> ${shape.render()} <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text></svg>`;
+    const svgFile = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    ${shape.render()}
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+</svg>`
 
     fs.writeFile('SVG-Generator.svg', svgFile, (err) => {
         if(err) throw err;
